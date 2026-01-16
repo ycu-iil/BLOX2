1c4822eafb1d37351cf8cdb9ae37b768e53a228e
satoru-fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2026-01-16 14:49:58 +0900

    Add docstring

--- blox2/blox2_selector.py
+++ blox2/blox2_selector.py
@@ -6,6 +6,10 @@ from .utils import stein_novelty_repli
 
 class BLOX2Selector(Selector):
     def __init__(self, observed_features: pd.DataFrame, observed_values: pd.DataFrame, unobserved_features: pd.DataFrame, predictor: Predictor, normalize_features: bool=True, normalize_values: bool=True, squared_sigma: float=1, n_obs_samples: int=None, n_chunks: int=256, use_distribution: bool=False, compare_selection_time=False):
+        """
+        Args:
+            n_obs_samples: When the number of observed points are greater than this value, samples n_obs_samples points for Stein novelty calculation instead of using all of the observed points.
+        """
         super().__init__(observed_features, observed_values, unobserved_features, predictor, squared_sigma=squared_sigma, normalize_features=normalize_features, normalize_values=normalize_values)
         self._use_distribution = use_distribution
         self.compare_selection_time = compare_selection_time
